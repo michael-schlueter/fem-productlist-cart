3f0d8b3f31988884fe86b52313d686eae244628c
Michael Schlueter <schluet0r4life@gmail.com>
2024-08-27 17:32:46 +0200

    apply typography

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,15 @@ function App() {
     <div>
       <main>
         <section>
-          <h1>Desserts</h1>
+          <h1 className="text-2xl font-bold">Desserts</h1>
           <ul>
             {desserts.map((dessert, index) => (
               <li key={index}>
                 <img src={dessert.image.mobile} alt={dessert.name} />
                 <CartButton />
-                <h2>{dessert.category}</h2>
-                <p>{dessert.name}</p>
-                <p>${dessert.price}</p>
+                <h2 className="text-sm text-rose-500">{dessert.category}</h2>
+                <p className="text-rose-900 font-semibold">{dessert.name}</p>
+                <p className="text-red font-semibold">${dessert.price}</p>
               </li>
             ))}
           </ul>
--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,10 +1,12 @@
 export default function Cart() {
   return (
     <section>
-      <h3>Your Cart (0)</h3>
+      <h3 className="text-xl font-bold text-red">Your Cart (0)</h3>
       <div>
         <img src="./public/assets/images/illustration-empty-cart.svg" alt="" />
-        <p>Your added items will appear here</p>
+        <p className="text-sm font-bold text-rose-500">
+          Your added items will appear here
+        </p>
       </div>
     </section>
   );
