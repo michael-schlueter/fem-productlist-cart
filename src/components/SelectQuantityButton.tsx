c0d36f99ba882641024a5f24255cd57f4877cc3e
Michael Schlueter <schluet0r4life@gmail.com>
2024-08-29 17:32:53 +0200

    adjust SelectQuantityButton position and border

--- src/components/SelectQuantityButton.tsx
+++ src/components/SelectQuantityButton.tsx
@@ -1,19 +1,20 @@
 export default function SelectQuantityButton() {
-    return (
-      <div className="w-40 h-[44px] rounded-full border bg-red-500 text-sm text-white font-bold flex items-center justify-between p-3 cursor-auto">
-        <button className="w-5 h-5 border border-white rounded-full flex items-center justify-center">
-          <img
-            src="./public/assets/images/icon-decrement-quantity.svg"
-            alt="decrement quantity"
-          />
-        </button>
-        1
-        <button className="w-5 h-5 border border-white rounded-full flex items-center justify-center">
-          <img
-            src="./public/assets/images/icon-increment-quantity.svg"
-            alt="increment quantity"
-          />
-        </button>
-      </div>
-    );
-  }
+  return (
+    // -mt-38 to offset gap and 1/2 height of button
+    <div className="w-40 h-[44px] rounded-full bg-red-500 text-sm text-white font-bold flex items-center justify-between p-3 cursor-auto self-center -mt-[38px]">
+      <button className="w-5 h-5 border border-white rounded-full flex items-center justify-center">
+        <img
+          src="./public/assets/images/icon-decrement-quantity.svg"
+          alt="decrement quantity"
+        />
+      </button>
+      1
+      <button className="w-5 h-5 border border-white rounded-full flex items-center justify-center">
+        <img
+          src="./public/assets/images/icon-increment-quantity.svg"
+          alt="increment quantity"
+        />
+      </button>
+    </div>
+  );
+}
