611c7039457edea31ad267b5ee2a73962a3f92d3
rulo <schluet0r4life@gmail.com>
2024-09-19 14:45:29 +0200

    apply max-width to container (desktop)

--- src/App.tsx
+++ src/App.tsx
@@ -47,8 +47,8 @@ function App() {
   };
 
   return (
-    <div className="bg-rose-50">
-      <main className="flex flex-col gap-8 p-6 sm:p-10 lg:grid lg:grid-cols-[2fr_1fr]">
+    <div className="bg-rose-50 lg:flex lg:items-center lg:justify-center">
+      <main className="max-w-[1260px] flex flex-col gap-8 p-6 sm:p-10 lg:grid lg:grid-cols-[2fr_1fr]">
         <CartList
           onAddCartItem={addCartItem}
           cartItems={cartItems}
