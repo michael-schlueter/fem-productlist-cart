e86b8e8bbb965adcae822f1e66f5350231cf814a
rulo <schluet0r4life@gmail.com>
2024-09-25 15:58:00 +0200

    testing setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+/// <reference types="vitest" />
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
